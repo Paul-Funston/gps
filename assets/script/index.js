2c55d08c0720f86130907d6e63e800a1065faf18
Paul Funston <paulfunston9@gmail.com>
2022-12-13 10:44:40 -0600

    fix height

--- assets/script/index.js
+++ assets/script/index.js
@@ -15,12 +15,39 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: [-97.1, 49.9], // starting position [lng, lat]
-    zoom: 8, // starting zoom
+    pitch: 40,
+    zoom: 16, // starting zoom
 });
 
+map.dragPan.disable();
+map.keyboard.disable();
+map.scrollZoom.disable();
+map.doubleClickZoom.disable();
+map.touchZoomRotate.disable();
 
+const marker = new mapboxgl.Marker({
+  color: '#3898ff'
+})
 
+function getLocation(position) {
+  const { longitude, latitude } = position.coords;
 
+  if (longitude && latitude) {
+    map.setCenter([longitude, latitude]);
+    marker.setLngLat([longitude, latitude]).addTo(map);
+    setTimeout(() => {clearCover()}, 750);
+  }
+}
+
+function errorHandler(event) {
+  stopSpin();
+  console.log(event.message);
+}
+
+const options = {
+  enableHighAccuracy: true,
+  maximumAge: 0,
+}
 
 
 function stopSpin() {
@@ -32,5 +59,20 @@ function clearCover() {
   const cover = document.querySelector('.map-cover');
   setTimeout(() => {
     cover.style.visibility = 'hidden';
-  }, 1000)
+  }, 500)
+}
+
+/*
+  The watchPosition() method is used to register a handler funtion that will
+  be called automatically each time the position of the device changes
+*/
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    getLocation, 
+    errorHandler,
+    options
+    )
+} else {
+  console.log(`Geolocation is not supported by your browser`);
 }
